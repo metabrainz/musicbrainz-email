bf7888f0d68a7fb2578d2a1a4ab4e8b91420b542
Oliver Charles <ollie@ocharles.org.uk>
2013-04-09 14:09:47 +0100

    Handle errors by re-queuening on failure queues

--- src/Main.hs
+++ src/Main.hs
@@ -2,13 +2,26 @@
 {-# LANGUAGE StandaloneDeriving #-}
 module Main (main) where
 
+--------------------------------------------------------------------------------
+import Prelude hiding (catch)
+
+
+--------------------------------------------------------------------------------
+import Control.Exception (SomeException, catch)
 import Control.Monad (forever, void)
 import Data.Data
 
+
+--------------------------------------------------------------------------------
 import qualified Data.Aeson.Generic as Aeson
+import qualified Data.ByteString.Lazy as LBS
 import qualified Network.AMQP as AMQP
 import qualified Network.Mail.Mime as Mail
+import qualified Data.Text as Text
+import qualified Data.Text.Encoding as Text
 
+
+--------------------------------------------------------------------------------
 deriving instance Data Mail.Address
 deriving instance Data Mail.Encoding
 deriving instance Data Mail.Mail
@@ -19,14 +32,51 @@ deriving instance Typeable Mail.Encoding
 deriving instance Typeable Mail.Mail
 deriving instance Typeable Mail.Part
 
-sendEmail :: AMQP.Message -> AMQP.Envelope -> IO ()
-sendEmail msg env = do
-  case Aeson.decode (AMQP.msgBody msg) of
-    Just mail -> Mail.renderSendMail mail
-    Nothing -> putStrLn "FAILED TO SEND AN EMAIL! OH MY GOD"
+--------------------------------------------------------------------------------
+-- | Takes a 'AMQP.Connection' and returns a callback that can be used on the
+-- outbox queue. To form the callback, IO is performed to open a 'AMQP.Channel'
+-- for the callback, so that it can publish failures.
+emailConsumer :: AMQP.Connection -> IO (AMQP.Message -> AMQP.Envelope -> IO ())
+emailConsumer rabbitMqConn = do
+  rabbitMq <- AMQP.openChannel rabbitMqConn
 
-  AMQP.ackEnv env
+  (invalidQueue, _, _) <- AMQP.declareQueue rabbitMq
+    AMQP.newQueue { AMQP.queueName = "outbox.invalid" }
 
+  (unroutableQueue, _, _) <- AMQP.declareQueue rabbitMq
+    AMQP.newQueue { AMQP.queueName = "outbox.unroutable" }
+
+  AMQP.declareExchange rabbitMq
+    AMQP.newExchange { AMQP.exchangeName = failureExchange
+                     , AMQP.exchangeType = "direct"
+                     }
+
+  AMQP.bindQueue rabbitMq invalidQueue failureExchange invalidKey
+  AMQP.bindQueue rabbitMq invalidQueue failureExchange unroutableKey
+
+  return $ \msg env -> do
+    case Aeson.decode (AMQP.msgBody msg) of
+      Just mail ->
+        Mail.renderSendMail mail `catch`
+          (\e -> let errorString = Text.pack $ show (e :: SomeException)
+                 in AMQP.publishMsg rabbitMq failureExchange unroutableKey
+                      AMQP.newMsg
+                        { AMQP.msgBody =
+                            LBS.fromChunks [ Text.encodeUtf8 errorString ]
+                        })
+      Nothing ->
+        AMQP.publishMsg rabbitMq failureExchange invalidKey msg
+
+    AMQP.ackEnv env
+
+ where
+
+  failureExchange = "failure"
+  invalidKey = "invalid"
+  unroutableKey = "unroutable"
+
+
+--------------------------------------------------------------------------------
 main :: IO ()
 main = do
   rabbitMqConn <- AMQP.openConnection "127.0.0.1" "/email" "guest" "guest"
@@ -42,7 +92,8 @@ main = do
 
   AMQP.bindQueue rabbitMq outboxQueue outboxExchange ""
 
-  AMQP.consumeMsgs rabbitMq outboxQueue AMQP.Ack (uncurry sendEmail)
+  consumer <- emailConsumer rabbitMqConn
+  AMQP.consumeMsgs rabbitMq outboxQueue AMQP.Ack (uncurry consumer)
 
   void $ forever getLine
  where
