87e296f3125c4986ef8c4c48a3672a292c42bdb3
Oliver Charles <ollie@ocharles.org.uk>
2013-04-09 16:28:21 +0100

    Use Heist to template emails

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE StandaloneDeriving #-}
 module Main (main) where
 
@@ -7,30 +8,84 @@ import Prelude hiding (catch)
 
 
 --------------------------------------------------------------------------------
+import Control.Applicative
 import Control.Exception (SomeException, catch)
-import Control.Monad (forever, void)
+import Control.Monad (forever, join, void)
+import Control.Monad.Trans.Either (runEitherT)
 import Data.Data
+import Data.Functor.Identity (Identity, runIdentity)
 
 
 --------------------------------------------------------------------------------
+import qualified Blaze.ByteString.Builder as Builder
 import qualified Data.Aeson.Generic as Aeson
+import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as LBS
-import qualified Network.AMQP as AMQP
-import qualified Network.Mail.Mime as Mail
 import qualified Data.Text as Text
 import qualified Data.Text.Encoding as Text
+import qualified Heist as Heist
+import qualified Heist.Interpreted as Heist
+import qualified Network.AMQP as AMQP
+import qualified Network.Mail.Mime as Mail
+import qualified Text.XmlHtml as XmlHtml
 
 
 --------------------------------------------------------------------------------
 deriving instance Data Mail.Address
-deriving instance Data Mail.Encoding
-deriving instance Data Mail.Mail
-deriving instance Data Mail.Part
-
 deriving instance Typeable Mail.Address
-deriving instance Typeable Mail.Encoding
-deriving instance Typeable Mail.Mail
-deriving instance Typeable Mail.Part
+
+data Email = Email
+    { emailTemplate :: Template
+    , emailTo :: Mail.Address
+    , emailFrom :: Mail.Address
+    }
+  deriving (Data, Typeable)
+
+
+--------------------------------------------------------------------------------
+data Template = PasswordReset { passwordResetEditor :: Text.Text }
+  deriving (Data, Typeable)
+
+
+--------------------------------------------------------------------------------
+emailToMail :: Email -> Heist.HeistState Identity -> Maybe Mail.Mail
+emailToMail email heist = runIdentity $
+  Heist.evalHeistT
+    mailBuilder
+    (XmlHtml.TextNode "")
+    (Heist.bindStrings templateBindings heist)
+
+ where
+
+  mailBuilder = fmap (makeMail . runTemplate) <$> Heist.evalTemplate templatePath
+
+  runTemplate = Builder.toLazyByteString . XmlHtml.renderHtmlFragment XmlHtml.UTF8
+
+  template = emailTemplate email
+
+  templatePath = case template of
+    (PasswordReset _) -> "password-reset"
+
+  templateBindings = case template of
+    (PasswordReset editor) -> [("editor", editor)]
+
+  emailSubject = case template of
+    (PasswordReset _) -> "Mandatory Password Reset"
+
+  makeMail messageBody = Mail.Mail
+    { Mail.mailFrom = emailFrom email
+    , Mail.mailTo = [ emailTo email ]
+    , Mail.mailCc = []
+    , Mail.mailBcc = []
+    , Mail.mailHeaders = [("Subject", emailSubject)]
+    , Mail.mailParts = [ [ Mail.Part { Mail.partType = "text/plain"
+                                     , Mail.partEncoding = Mail.None
+                                     , Mail.partFilename = Nothing
+                                     , Mail.partHeaders = []
+                                     , Mail.partContent = messageBody
+                                     } ] ]
+    }
+
 
 --------------------------------------------------------------------------------
 -- | Takes a 'AMQP.Connection' and returns a callback that can be used on the
@@ -54,18 +109,30 @@ emailConsumer rabbitMqConn = do
   AMQP.bindQueue rabbitMq invalidQueue failureExchange invalidKey
   AMQP.bindQueue rabbitMq invalidQueue failureExchange unroutableKey
 
+  heist <- fmap (either (error . show) id) $ runEitherT $ do
+      templateRepo <- Heist.loadTemplates "templates"
+      Heist.initHeist (Heist.HeistConfig [] [] [] [] templateRepo)
+
   return $ \msg env -> do
     case Aeson.decode (AMQP.msgBody msg) of
-      Just mail ->
-        Mail.renderSendMail mail `catch`
-          (\e -> let errorString = Text.pack $ show (e :: SomeException)
-                 in AMQP.publishMsg rabbitMq failureExchange unroutableKey
-                      AMQP.newMsg
-                        { AMQP.msgBody =
-                            LBS.fromChunks [ Text.encodeUtf8 errorString ]
-                        })
+      -- JSON decoding failed
       Nothing ->
         AMQP.publishMsg rabbitMq failureExchange invalidKey msg
+
+      Just email -> do
+        case emailToMail email heist of
+          -- Template rendering failed
+          Nothing ->
+            AMQP.publishMsg rabbitMq failureExchange invalidKey msg
+
+          Just mail ->
+            Mail.renderSendMail mail `catch`
+              (\e -> let errorString = Text.pack $ show (e :: SomeException)
+                     in AMQP.publishMsg rabbitMq failureExchange unroutableKey
+                          AMQP.newMsg
+                            { AMQP.msgBody =
+                                LBS.fromChunks [ Text.encodeUtf8 errorString ]
+                            })
 
     AMQP.ackEnv env
 
---
+++ templates/password-reset.tpl
@@ -0,0 +1,36 @@
+Dear <editor />,
+
+On March 29th 2013 we discovered that one of the MusicBrainz database dumps
+contained password hashes for a large portion of MusicBrainz accounts. While we
+don't believe that these password hashes are either useful or widely
+distributed, we are requiring all users change their passwords.
+
+The database dumps that contain this data were promptly deleted, and have been
+replaced with correctly sanitized database dumps. Unfortunately logs from this
+server do show that this database dump was downloaded, and as we have no real
+indication of where this data now is, we're treating this seriously. We have
+adjusted our database dumping scripts to be very specific about exactly which
+data they should export, so that in the future we will not leak private data by
+making the same mistake again.
+
+We're extremely sorry about this mistake, and while we don't believe this data
+should allow attackers to retrieve user passwords, we can't be 100% certain. As
+such, we require that all users change their password as soon as possible.
+
+The next time you login to the website, you will be requested to change your
+password. Alternatively, you can go to the following link:
+
+    https://musicbrainz.org/account/change-password?mandatory=1&username=<editor />
+
+Users should also note that access to authenticated web service calls, for
+example to manage tags and ratings via Picard, are also blocked until passwords
+are changed. If you are finding that software that uses MusicBrainz is not
+behaving as youd expect, please check that you can login via the website.
+
+For more details, please see the blog post:
+
+    http://blog.musicbrainz.org/?p=1844
+
+We're extremely sorry this happened, and thank you for your co-operation.
+
+- The MusicBrainz Team
