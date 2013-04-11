705a12e7c9398db93cbc9a44307e0335eeedfcac
Oliver Charles <ollie@ocharles.org.uk>
2013-04-11 14:26:28 +0100

    Allow the emailer daemon to take a configurable RabbitMQ/statsd connection

--- mailer/Main.hs
+++ mailer/Main.hs
@@ -1,30 +1,46 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 module Main (main) where
 
 --------------------------------------------------------------------------------
+import Control.Applicative ((<$>), (<*>), (<**>))
 import Control.Monad (forever, void)
+import Data.Monoid (mconcat, mempty)
 
 
 --------------------------------------------------------------------------------
 import qualified Network.AMQP as AMQP
 import qualified Network.Mail.Mime as Mail
 import qualified Network.Metric as Metrics
-
+import qualified Options.Applicative as Optparse
+import qualified Network.Socket as Socket
 
 --------------------------------------------------------------------------------
 import qualified Mailer
 import qualified MusicBrainz.Email as Email
+import qualified MusicBrainz.Messaging as Messaging
 import qualified RateLimit
 
+
 --------------------------------------------------------------------------------
-main :: IO ()
-main = do
-  rabbitMqConn <- AMQP.openConnection "127.0.0.1" "/email" "guest" "guest"
+data StatsdConfiguration = Statsd { statsdHost :: String
+                                  , statsdPort :: Socket.PortNumber
+                                  }
+
+
+--------------------------------------------------------------------------------
+data Options = Options Messaging.RabbitMQConnection StatsdConfiguration
+
+
+--------------------------------------------------------------------------------
+run :: Options -> IO ()
+run (Options rabbitMqConf Statsd {..}) = do
+  rabbitMqConn <- Messaging.connect rabbitMqConf
   rabbitMq <- AMQP.openChannel rabbitMqConn
 
   Email.establishRabbitMqConfiguration rabbitMq
 
-  statsd <- Metrics.open Metrics.Statsd "musicbrainz" "10.1.1.105" 8125
+  statsd <- Metrics.open Metrics.Statsd "musicbrainz" statsdHost statsdPort
   sendMail <- let approximateEditorCount = 680000
                   day = 24 * 60 * 60.0
               in RateLimit.rateLimit (approximateEditorCount / day) $
@@ -37,3 +53,28 @@ main = do
   Mailer.consumeOutbox rabbitMqConn heist sendMail
 
   void $ forever getLine
+
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main = Optparse.execParser parser >>= run
+
+ where
+
+  parser =
+    Optparse.info
+      (Options <$> Messaging.rabbitOptparse
+               <*> statsdParser
+               <**> Optparse.helper)
+          mempty
+
+  statsdParser =
+    Statsd
+      <$> Optparse.strOption (mconcat [ Optparse.long "statsd-host"
+                                      , Optparse.help "Statsd host"
+                                      ])
+      <*> fmap Socket.PortNum
+            (Optparse.option (mconcat [ Optparse.long "statsd-port"
+                                      , Optparse.value 8125
+                                      , Optparse.help "Statsd port"
+                                      ]))
