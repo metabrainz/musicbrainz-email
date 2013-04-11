8db539d62e7539209e587cf5445d5972c7b12c89
Oliver Charles <ollie@ocharles.org.uk>
2013-04-11 14:21:42 +0100

    Record a metric of emails sucessfully sent

--- mailer/Main.hs
+++ mailer/Main.hs
@@ -6,8 +6,9 @@ import Control.Monad (forever, void)
 
 
 --------------------------------------------------------------------------------
-import qualified Network.Mail.Mime as Mail
 import qualified Network.AMQP as AMQP
+import qualified Network.Mail.Mime as Mail
+import qualified Network.Metric as Metrics
 
 
 --------------------------------------------------------------------------------
@@ -23,10 +24,14 @@ main = do
 
   Email.establishRabbitMqConfiguration rabbitMq
 
+  statsd <- Metrics.open Metrics.Statsd "musicbrainz" "10.1.1.105" 8125
   sendMail <- let approximateEditorCount = 680000
                   day = 24 * 60 * 60.0
-              in RateLimit.rateLimit (approximateEditorCount / day)
-                   Mail.renderSendMail
+              in RateLimit.rateLimit (approximateEditorCount / day) $
+                   \mail -> do
+                     Mail.renderSendMail mail
+                     Metrics.push statsd $
+                       Metrics.Counter "email" "sent" 1
 
   heist <- Mailer.loadTemplates
   Mailer.consumeOutbox rabbitMqConn heist sendMail
