f903ddca6255752203753af51a1548419afd23bd
Michael Wiencek <mwtuea@gmail.com>
2015-12-04 16:48:50 -0600

    Tighten mime-mail version, fix overlapping instances

--- src/MusicBrainz/Email.hs
+++ src/MusicBrainz/Email.hs
@@ -37,9 +37,7 @@ import qualified Network.Mail.Mime as Mail
 
 
 --------------------------------------------------------------------------------
-deriving instance Eq Mail.Address
 deriving instance Generic Mail.Address
-deriving instance Show Mail.Address
 
 data Email = Email
     { emailTemplate :: Template
