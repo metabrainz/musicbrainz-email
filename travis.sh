3f66265a2a57065019082fc6e67ceb7084761753
Michael Wiencek <mwtuea@gmail.com>
2015-12-05 01:03:13 -0600

    Setup email vhost

--- travis.sh
+++ travis.sh
@@ -15,6 +15,9 @@ psql -U postgres musicbrainz_email -c "CREATE EXTENSION \"uuid-ossp\""
 psql -U postgres musicbrainz_email -c "CREATE EXTENSION musicbrainz_collate"
 psql -U musicbrainz musicbrainz_email -c "CREATE SCHEMA musicbrainz"
 psql -U musicbrainz musicbrainz_email < admin/sql/CreateTables.sql
+
+sudo rabbitmqctl add_vhost email
+sudo rabbitmqctl set_permissions -p /email guest '.*' '.*' '.*'
 popd
 
 cabal update
